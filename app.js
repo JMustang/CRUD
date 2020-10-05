55e0a8e4dd953cbd40e19b13fec781ef4160abe7
junior <eecfredes@gmail.com>
2020-10-04 23:16:44 -0300

    finished

--- app.js
+++ app.js
@@ -9,25 +9,25 @@ var form = document.getElementById("Formulario");
 var isEditing = false;
 var Posicao;
 
-start = () => {
+function start() {
     PrevenirComportamentoDefault(form);
     AplicarFoco(Input);
     CapiturarValoresDigitados(Input);
     ExibirVetor();
-}
+};
 
-PrevenirComportamentoDefault = (Objeto) => {
+function PrevenirComportamentoDefault(Objeto) {
     Objeto.addEventListener("submit", (event) => {
         event.preventDefault();
     });
-}
+};
 
-AplicarFoco = (Objeto) => {
+function AplicarFoco(Objeto) {
     Objeto.focus();
-}
+};
 
-CapiturarValoresDigitados = (Objeto) => {
-    Objeto.addEventListener("keyup", (event) => {
+function CapiturarValoresDigitados(Objeto) {
+    Objeto.addEventListener("keyup", function (event) {
         if (event.key === "Enter") {
             var ValorDigitado = event.target.value;
 
@@ -44,4 +44,60 @@ CapiturarValoresDigitados = (Objeto) => {
             ExibirVetor();
         }
     });
-}
+};
+
+function ExibirVetor() {
+    ul.innerHTML = "";
+    Input.value = "";
+
+    GlobalNames.forEach(PercorrerVetor);
+    nomes.appendChild(ul);
+};
+
+function PercorrerVetor(item) {
+    var li = document.createElement("li");
+
+    li.appendChild(CriarBotao());
+    li.appendChild(CriarSpan(item));
+    ul.appendChild(li);
+};
+
+function CriarBotao() {
+    var botao = document.createElement("button");
+    botao.classList.add("DeleteButton");
+    botao.textContent = "x";
+
+    return botao;
+};
+
+function CriarSpan(Valor) {
+    var span = document.createElement("span");
+    span.textContent = Valor;
+    span.classList.add("clicavel");
+    span.addEventListener("click", EditarItem);
+
+    return span;
+};
+
+function EditarItem(event) {
+    var valor = event.target.innerHTML;
+
+    var index = GlobalNames.indexOf(valor);
+    Input.value = GlobalNames[index];
+    AplicarFoco(Input);
+    isEditing = true;
+    Posicao = index;
+};
+
+ul.addEventListener("click", function (event) {
+    if (event.target.localName === "button") {
+        var valor = event.srcElement.nextElementSibling.innerHTML;
+
+        var index = GlobalNames.indexOf(valor);
+        GlobalNames.splice(index, 1);
+
+        var ancestral = event.target.parentElement;
+        ancestral.remove();
+        ExibirVetor();
+    }
+});
